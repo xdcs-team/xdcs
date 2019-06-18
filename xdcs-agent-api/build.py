63633e2b8a474f12f09fecdf2c6954af17a1a9f0
Kamil Jarosz <kamil09875@gmail.com>
2019-06-18 22:40:55 +0200

    PPXDCS-19 Add REST API definition

--- xdcs-agent-api/build.py
+++ xdcs-agent-api/build.py
@@ -1,6 +1,7 @@
-from pybuilder.core import use_plugin, init, depends, task, dependents
-from pybuilder.plugins.core_plugin import clean
 import os
+
+from pybuilder.core import use_plugin, init, task, dependents
+from pybuilder.plugins.core_plugin import clean
 
 use_plugin("python.install_dependencies")
 use_plugin("python.distutils")
@@ -18,8 +19,14 @@ def initialize(project):
     project.set_property('dir_source_main_python', 'target/generated-sources/protobuf/python')
     os.makedirs('./target/generated-sources/protobuf/python')
     os.makedirs('./target/generated-sources/protobuf/grpc-python')
-    project.set_property('package_command', 'python3 -m grpc_tools.protoc -I=src/main/proto --python_out=./target/generated-sources/protobuf/python --grpc_python_out=./target/generated-sources/protobuf/grpc-python ./src/main/proto/agent_api.proto')
+    project.set_property(
+        'package_command',
+        'python3 -m grpc_tools.protoc -I=src/main/proto '
+        '--python_out=./target/generated-sources/protobuf/python '
+        '--grpc_python_out=./target/generated-sources/protobuf/grpc-python '
+        './src/main/proto/agent_api.proto')
     project.set_property('clean_command', 'rm -r ./target/generated-sources/protobuf')
+
 
 @task
 @dependents(clean)
