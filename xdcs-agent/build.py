ee891a18ca5f11e33bd0b382afcecfecd3ebccbe
Kamil Jarosz <kamil09875@gmail.com>
2019-07-07 20:25:06 +0200

    PPXDCS-24 Set up Docker and Travis

--- xdcs-agent/build.py
+++ xdcs-agent/build.py
@@ -1,42 +1,61 @@
 import os
+import shutil
+import sys
 
 from pybuilder.core import use_plugin, init, task, dependents
-from pybuilder.plugins.core_plugin import clean
+from pybuilder.plugins.core_plugin import clean, compile_sources
 
 use_plugin("python.core")
 use_plugin("python.unittest")
 use_plugin("python.install_dependencies")
 use_plugin("python.distutils")
-use_plugin('python.pycharm')
+use_plugin("python.pycharm")
 use_plugin("exec")
 
 name = "xdcs-agent"
 
-version = '0.1.0.dev'
+version = "0.1.0.dev"
 default_task = "publish"
+
+
+@init
+def check_version():
+    if sys.version_info[0] < 3:
+        raise Exception("Only python 3 is supported")
 
 
 @init
 def initialize(project):
     project.build_depends_on('mockito')
     project.build_depends_on('protobuf')
+    project.build_depends_on('grpcio')
     project.build_depends_on('grpcio-tools')
 
-    os.makedirs('./target/generated-sources/protobuf/python', exist_ok=True)
-    os.makedirs('./target/generated-sources/protobuf/grpc-python', exist_ok=True)
-    project.set_property(
-        'package_command',
-        'python3 '
-        '-m grpc_tools.protoc '
-        '-I=src/main/proto '
-        '--python_out=./target/generated-sources/protobuf/python '
-        '--grpc_python_out=./target/generated-sources/protobuf/python '
-        './src/main/proto/xdcs_api/agent_api.proto')
-    project.set_property('clean_command', 'rm -r ./target/generated-sources/protobuf')
+    project.set_property('distutils_packages', [
+        "xdcs"])
+    project.set_property('distutils_console_scripts', [
+        "xdcs-agent = xdcs.agent_cli:main"])
+
+
+@task
+@dependents(compile_sources)
+def compile_grpc():
+    from grpc_tools import protoc
+    import pkg_resources
+
+    gen_python = './target/generated-sources/protobuf/python'
+
+    os.makedirs(gen_python, exist_ok=True)
+    proto_include = pkg_resources.resource_filename('grpc_tools', '_proto')
+    protoc.main([sys.argv[0],
+                 '-I=src/main/proto',
+                 '--python_out=' + gen_python,
+                 '--grpc_python_out=' + gen_python,
+                 './src/main/proto/xdcs_api/agent_api.proto',
+                 '-I{}'.format(proto_include)])
 
 
 @task
 @dependents(clean)
-def deploy_unittests_resources(project):
-    os.removedirs('./target/generated-sources/protobuf/grpc-python')
-    pass
+def clean_grpc():
+    shutil.rmtree('./target/generated-sources/protobuf')
