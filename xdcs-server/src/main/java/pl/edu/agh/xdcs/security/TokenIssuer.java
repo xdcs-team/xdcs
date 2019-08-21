e7c7d72767a92afb5ce6f0a03dc8b74d44600058
Kamil Jarosz <kamil09875@gmail.com>
2019-08-21 22:35:34 +0200

    PPXDCS-62 Add configuration for JWT keys

--- xdcs-server/src/main/java/pl/edu/agh/xdcs/security/TokenIssuer.java
+++ xdcs-server/src/main/java/pl/edu/agh/xdcs/security/TokenIssuer.java
@@ -5,8 +5,19 @@ import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import pl.edu.agh.xdcs.config.AgentSecurityConfiguration;
+import pl.edu.agh.xdcs.config.Configured;
+import pl.edu.agh.xdcs.config.JwtKeyConfiguration;
+import pl.edu.agh.xdcs.config.WebSecurityConfiguration;
+import pl.edu.agh.xdcs.config.util.ReferencedFileLoader;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.security.Key;
 import java.sql.Date;
 import java.time.Duration;
@@ -20,7 +31,58 @@ import java.util.Optional;
  */
 @ApplicationScoped
 public class TokenIssuer {
-    private Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private Key webKey;
+    private Key agentKey;
+
+    @Inject
+    @Configured
+    private WebSecurityConfiguration webSecurityConfiguration;
+
+    @Inject
+    @Configured
+    private AgentSecurityConfiguration agentSecurityConfiguration;
+
+    @Inject
+    private ReferencedFileLoader fileLoader;
+
+    @PostConstruct
+    private void loadKeys() {
+        try {
+            webKey = loadOrCreateKey(webSecurityConfiguration.getJwtKey());
+            agentKey = loadOrCreateKey(agentSecurityConfiguration.getJwtKey());
+        } catch (IOException e) {
+            throw new UncheckedIOException("IO error while reading JWT keys", e);
+        }
+    }
+
+    private Key loadOrCreateKey(JwtKeyConfiguration config) throws IOException {
+        Path path = Optional.ofNullable(config)
+                .map(JwtKeyConfiguration::getPath)
+                .map(fileLoader::toPath)
+                .orElse(null);
+
+        if (path == null) {
+            return Keys.secretKeyFor(SignatureAlgorithm.HS512);
+        }
+
+        if (Files.isReadable(path)) {
+            byte[] encoded = Files.readAllBytes(path);
+            return Keys.hmacShaKeyFor(encoded);
+        }
+
+        if (Files.exists(path)) {
+            throw new IOException("File " + path + " exists but is not a regular file");
+        }
+
+        Key key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+        byte[] encoded = key.getEncoded();
+        Files.write(path, encoded);
+        return key;
+    }
+
+    private Key getKey(TokenType type) {
+        return type == TokenType.AGENT ? agentKey : webKey;
+    }
 
     public String issueToken(String user, Duration expirationTime, TokenType type) {
         return issueToken(user, expirationTime, type, Collections.emptyMap());
@@ -34,13 +96,13 @@ public class TokenIssuer {
                 .setExpiration(expirationDate)
                 .claim("type", type.toString())
                 .addClaims(claims)
-                .signWith(key)
+                .signWith(getKey(type))
                 .compact();
     }
 
     public Optional<Token> validateToken(String token, TokenType expectedType) {
         try {
-            Claims claims = Jwts.parser().setSigningKey(key)
+            Claims claims = Jwts.parser().setSigningKey(getKey(expectedType))
                     .parseClaimsJws(token)
                     .getBody();
 
